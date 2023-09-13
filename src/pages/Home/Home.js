e6a26119af0928963399c1549e902b5ab20db3de
huycuong23 <cuongcuonghuy@gmail.com>
2023-09-13 21:18:50 +0700

    update prop for more info account navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,33 +1,32 @@
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
+import Image from '~/components/Image/Image';
+import Button from '~/components/Button/Button';
+
+const cx = classNames.bind(styles);
 function Home() {
     return (
-        <div>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-             <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
-            <h2>Homepage</h2>
+        <div className={cx('container')}>
+            <header className={cx('header')}>
+                <Image className={cx('avatar')} alt="avatar" />
+                <div className={cx('header-info')}>
+                    <div>
+                        <div className={cx('user-name')}>
+                            <span className={cx('name')}>
+                                <span>{'name'}</span>
+                                {/* {data.tick && (
+                            <FontAwesomeIcon className={cx('check-circle')} icon={faCheckCircle} />
+                        )} */}
+                            </span>
+                            <span className={cx('user-name')}>{'nickname'}</span>
+                        </div>
+                        <p className={cx('title')}>Title</p>
+                    </div>
+                    <Button className={cx('button')} primary outline>
+                        Follow
+                    </Button>
+                </div>
+            </header>
         </div>
     );
 }
